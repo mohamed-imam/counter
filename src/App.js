54160e0fb42da3bdd72bb1c8e5a672e0ba74afbc
Muhammed Imam <mohamed.imam91997@gmail.com>
2022-06-28 15:37:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/CounterBtn";
+import "./assets/css/style.css";
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let increment = () => {
+    setCount(count + 1);
+  };
+
+  let decrement = () => {
+    setCount((count) => Math.max(count - 1, 0));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div>
+        <h1>Counter App</h1>
+        <div class='count'>
+          <h3>Count: </h3>
+          <h1>{count}</h1>
+        </div>
+        <div class='buttons'>
+          <Button title={"+"} action={increment} />
+          <Button title={"-"} action={decrement} />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
